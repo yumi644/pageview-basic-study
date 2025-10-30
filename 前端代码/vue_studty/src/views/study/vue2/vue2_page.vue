a2836d21b657be71d5fed249592bd3421cb0c5ac
yumi644 <71575079+yumi644@users.noreply.github.com>
2025-10-30 19:44:15 +0800

    新建两个vue文件

---
+++ 前端代码/vue_studty/src/views/study/vue2/vue2_page.vue
@@ -0,0 +1,19 @@
+<<template>
+    <div>
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Vue2Page",
+  data() {
+    return {
+      msg: "Hello Vue2",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
